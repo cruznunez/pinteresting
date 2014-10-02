c05059180020aed34d8618cb67a2a48a29461bd6
Cruz Nunez <nunez.a.cruz@gmail.com>
2014-10-02 18:58:03 -0400

    color change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+    #Required For Heroku
+    #Set this to actual host name
+  config.action_mailer.default_url_options = { :host => 'http://omr-pinteresting-cruz.com/' }
 end
