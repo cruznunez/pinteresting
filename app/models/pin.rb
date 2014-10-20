1c374eb69611132af2d247218d2b47346b8e4eb3
Cruz Nunez <nunez.a.cruz@gmail.com>
2014-10-19 23:54:00 -0400

    fluid images

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
-     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+     has_attached_file :image, :styles => { :large => "500x500>", :medium => "300x300>", :thumb => "100x100>" }
 
      validates :description, presence: true
 
